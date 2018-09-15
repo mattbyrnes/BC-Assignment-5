fa5944a23084d54685d77cc4afd75cffb0440db5
Matt Byrnes <mattjbyrnes@gmail.com>
2018-09-15 09:38:12 -0400

    Sat Commit

---
+++ Readme.md
@@ -0,0 +1,58 @@
+# Homework 4 - UI for Employee List Redesign, Practice Problem, and Reading
+
+There are **3 parts** to this homework. The homework is only complete if **all** parts are submitted.
+
+Homework is only incomplete if 1 or more parts are not submitted. Non-working code is **not** an incomplete. Please turn in any code you have. 
+
+**This homework is due Tuesday September 18st at 5pm.** Late submissions are not accepted.
+
+## Part 1: Redesign UI for Employee List
+
+For the main part of this week's homework, you're going to be polishing up last week's assignment with a cleaner UI and cleaner code.
+
+There are two parts to this week's redo. They are as follows:
+
+### Match the new design
+
+Match the designs to look like the following images:
+
+![Home Page Desktop](images/home.png)
+
+![General Page Desktop](images/page.png)
+
+You will need to use [Font Awesome](https://fontawesome.com/) for the icons.
+
+### Clean up the code
+
+- Replace all for loops with `forEach`, `map`, `filter`, `find`, `some`, `every`, `findIndex`, or `includes`
+- Use a switch statement for the primary logic
+- All rendering takes place in a render function
+- No global variables other than functions and state (so command and employeeList should be on state).
+
+---
+
+## Part 2: Make More Buttons
+
+Using the code in `practice_problem.zip` do the following:
+
+The following code should add a new button with a unique number every time the more button is clicked and render 'Make More Buttons'. This functionality works perfectly! But it also should render the number of the new buttons when one of them is clicked. This doesn't work :(. Research how to add event listeners to dynamically added buttons and fix the code.
+
+When you have succeeded, pressing the `1` button, should render '1' to the page. Pressing the `2` button, should render '2' to the page, and so on.
+
+Read the commented code carefully and make sure you understand the application before you begin.
+
+---
+
+## Part 3: Introduction to Node.js Reading
+
+Read the following article: [What exactly is Node.js?](https://medium.freecodecamp.org/what-exactly-is-node-js-ae36e97449f5)
+
+* You may need to read the article several times to fully understand it. 
+
+* You may need to Google concepts that you don't fully understand. 
+
+* When you fully understand the article, write a synopsis as though you are explaining the concepts to a ten year old. 
+
+* Your summary must be at least 150 words.
+
+* Your summary must include one analogy. Example "A conditional is like a fork in the road. You go one way if you want to go home and another if you want to go to work."
---
+++ app.js
@@ -0,0 +1,152 @@
+//Print
+function runPrint() {
+  $('#output').empty();
+  for (i = 0; i < employeeList.length; i++) {
+    $('#output').append(`<p>Name: ${employeeList[i].name}</p>`);
+    $('#output').append(`<p>Office Number: ${employeeList[i].officeNum}</p>`);
+    $('#output').append(`<p>Phone Number: ${employeeList[i].phoneNum}</p><br />`);
+  }
+};
+
+
+const viewPrint = event => {
+  // $('#content').empty();
+  $('#input').append(`
+      <h1>Print</h1>
+      <p>PRINT all employees</p>
+      <button id="submit-print">Print</button>
+    `);
+    $('#submit-print').on('click', runPrint);
+};
+$('#menu-print').on('click', viewPrint);
+
+
+//Verify
+const viewVerify = event => {
+  $('#content, #output').empty();
+  $('#content').append(`
+      <h1>Verify</h1>
+      <p>Enter the name to VERIFY</p>
+      <input id="inputName" type="text">
+      <button id="submit-verify">Verify</button>
+    `);
+  function runVerify() {
+    $('#output').empty();
+    $('#output').append(`<p>No, this is not an employee.</p>`);
+    let userInput = document.querySelectorAll('#inputName')[0].value;
+    for (i = 0; i < employeeList.length; i++) {
+      if (userInput === employeeList[i].name.toLowerCase()) {
+        $('#output').empty();
+        $('#output').append(`<p>Yes, ${userInput} is an employee.</p>`);
+      }
+    }
+  };
+  $('#submit-verify').on('click', runVerify);
+};
+$('#menu-verify').on('click', viewVerify);
+
+//Lookup
+const viewLookup = event => {
+  $('#content, #output').empty();
+  $('#content').append(`
+      <h1>Lookup</h1>
+      <p>Enter the name to LOOKUP</p>
+      <input id="inputName" type="text">
+      <button id="submit-lookup">Lookup</button>
+    `);
+  function runLookup() {
+    $('#output').append(`<p>Employee Not Found</p>`);
+    let userInput = document.querySelectorAll('#inputName')[0].value;
+    for (let i = 0; i < employeeList.length; i++) {
+      if (userInput === employeeList[i].name.toLowerCase()) {
+        $('#output').empty();
+        $('#output').append(`<p>Name: ${employeeList[i].name}</p>`);
+        $('#output').append(`<p>Office: ${employeeList[i].officeNum}</p>`);
+        $('#output').append(`<p>Phone: ${employeeList[i].phoneNum}</p>`);
+      }
+    }
+  }
+  $('#submit-lookup').on('click', runLookup);
+};
+$('#menu-lookup').on('click', viewLookup);
+
+//Contains
+const viewContains = event => {
+  $('#content, #output').empty();
+  $('#content').append(`
+      <h1>Contains</h1>
+      <p>Employee name CONTAINS what letters?</p>
+      <input id="inputName" type="text">
+      <button id="submit-contains">Contains</button>
+    `);
+  function runContains() {
+    $('#output').append(`<p>Employee Not Found</p>`);
+    $('#output').empty();
+    let userInput = document.querySelectorAll('#inputName')[0].value;
+    for (i = 0; i < employeeList.length; i++) {
+      if (employeeList[i].name.toLowerCase().includes(userInput)) {
+        $('#output').append(`<p>Name: ${employeeList[i].name}</p>`);
+        $('#output').append(`<p>Office Number: ${employeeList[i].officeNum}</p>`);
+        $('#output').append(`<p>Phone Number: ${employeeList[i].phoneNum}</p><hr />`);
+      }
+    }
+  }
+  $('#submit-contains').on('click', runContains);
+};
+$('#menu-contains').on('click', viewContains);
+
+//Update//////////////////////////
+
+//Add Section
+
+const runAdd = event => {
+  let nameAdd = document.querySelectorAll('#newName')[0].value;
+  let officeAdd = document.querySelectorAll('#newOffice')[0].value;
+  let phoneAdd = document.querySelectorAll('#newPhone')[0].value;
+  var employeeAdd = { name: nameAdd, officeNum: officeAdd, phoneNum: phoneAdd };
+  employeeList.push(employeeAdd);
+  $('#output').empty();
+  $('#output').append(`${nameAdd} has been added.<br /><br />`);
+  runPrint();
+};
+
+const viewAdd = event => {
+  $('#content, #output').empty();
+  $('#content').append(`
+      <h1>Add</h1>
+      <p>ADD a new employee</p>
+      <p>Name: <input id="newName" type="text"></p>
+      <p>Office: <input id="newOffice" type="text"></p>
+      <p>Phone: <input id="newPhone" type="text"></p>
+      <button id="submit-add">Add</button></p>
+    `);
+    $('#submit-add').on('click', runAdd);
+};
+$('#menu-add').on('click', viewAdd);
+
+
+//Delete
+function runDelete() {
+  let userInput = document.querySelectorAll('#deleteName')[0].value;
+  for (i = 0; i < employeeList.length; i++) {
+    if (userInput === employeeList[i].name.toLowerCase()) {
+      employeeList.splice(i, 1);
+    }
+    $('#message').append(`${userInput} has been deleted.`);
+    runPrint();
+  }
+}
+
+const viewDelete = event => {
+  $('#content, #output').empty();
+  $('#content').append(`
+      <h1>Delete</h1>
+      <p>DELETE an employee</p>
+      <input id="deleteName" type="text">
+      <button id="submit-delete">Delete</button>
+    `);
+  $('#submit-delete').on('click', runDelete);
+};
+
+$('#menu-delete').on('click', viewDelete);
+
